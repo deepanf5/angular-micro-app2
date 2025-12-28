72529387781226a7621118b66e2a17d4f3d4ea28
deepan <deepanappu1999@gmail.com>
2025-12-28 13:29:38 +0530

    displayaing the data from movieList details micro apps

--- src/app/components/movie-details/movie-details.ts
+++ src/app/components/movie-details/movie-details.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,6 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './movie-details.html',
   styleUrl: './movie-details.css',
 })
-export class MovieDetails {
+export class MovieDetails implements OnInit, OnDestroy {
+
+  selectedMovie: any
+  constructor(private cdr: ChangeDetectorRef) { }
+
+
+  private eventHandler = (event: any) => {
+    this.selectedMovie = event.detail.movie;
+    this.cdr.detectChanges(); // Ensure UI updates in 2025 async contexts
+  };
+
+  ngOnInit(): void {
+    window.addEventListener('MOVIE_SELECTED', this.eventHandler);
+  }
+
+
+  ngOnDestroy() {
+    // Essential: prevent memory leaks in long-running 2025 apps
+    window.removeEventListener('MOVIE_SELECTED', this.eventHandler);
+  }
+
+
 
 }
